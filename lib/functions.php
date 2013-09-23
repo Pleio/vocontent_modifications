cfbc698dc4e3b6c004246971503d5e9efe487fdb
Jerome Bakker <jeabakker@coldtrick.com>
2013-09-23 13:25:25 +0200

    changed: access logit to only emplyees

--- lib/functions.php
+++ lib/functions.php
@@ -4,22 +4,44 @@
 		$valid = false;
 		
 		if (!empty($user) && elgg_instanceof($user, "user")) {
-			// first validate SAML attribute / value combination
-			$valid = vocontent_modifications_saml_attributes_validation($user);
-			
-			// next online validation
-			if (!$valid) {
-				$valid = vocontent_modifications_online_validation($user);
+			// only SAML employee's are allowed special access
+			if (vocontent_modification_employee_validation($user)) {
+				// first validate SAML attribute / value combination
+				$valid = vocontent_modifications_saml_attributes_validation($user);
+				
+				// next online validation
+				if (!$valid) {
+					$valid = vocontent_modifications_online_validation($user);
+				}
+				
+				// add or remove the user from the access list
+				if ($valid) {
+					vocontent_modifications_grant_access($user);
+				} else {
+					vocontent_modifications_revoke_access($user);
+				}
 			}
-			
-			// add or remove the user from the access list
-			if ($valid) {
-				vocontent_modifications_grant_access($user);
-			} else {
-				vocontent_modifications_revoke_access($user);
-			}
-			
 		}
+	}
+	
+	function vocontent_modification_employee_validation(ElggUser $user) {
+		$result = false;
+		
+		if (!empty($user) && elgg_instanceof($user, "user")) {
+			if (elgg_is_active_plugin("simplesaml")) {
+				$saml_source = elgg_get_plugin_setting("saml_source", "vocontent_modifications");
+				$affiliation_attribute = "eduPersonAffiliation";
+				
+				$affiliation_value = simplesaml_get_authentication_user_attribute($saml_source, $affiliation_attribute, $user->getGUID());
+				
+				// only staff is allowed this validation
+				if (!empty($affiliation_value) && in_array($affiliation_value, array("employee"))) {
+					$result = true;
+				}
+			}
+		}
+		
+		return $result;
 	}
 	
 	function vocontent_modifications_saml_attributes_validation(ElggUser $user) {
@@ -51,42 +73,36 @@
 			if (elgg_is_active_plugin("simplesaml")) {
 				$saml_source = elgg_get_plugin_setting("saml_source", "vocontent_modifications");
 				$brin_attribute = "nlEduPersonHomeOrganizationId";
-				$affiliation_attribute = "eduPersonAffiliation";
 				
 				$validation_url = elgg_get_plugin_setting("validation_url", "vocontent_modifications");
 				
 				// can we validate against a source
 				if (!empty($saml_source) && !empty($validation_url)) {
-					$affiliation_value = simplesaml_get_authentication_user_attribute($saml_source, $affiliation_attribute, $user->getGUID());
 					$saml_value = simplesaml_get_authentication_user_attribute($saml_source, $brin_attribute, $user->getGUID());
 					
-					// only staff is allowed this validation
-					if (!empty($affiliation_value) && in_array($affiliation_value, array("employee", "staff"))) {
+					if (!empty($saml_value)) {
+						if (!stristr($validation_url, "?id=")) {
+							$validation_url .= "?id=" . $saml_value;
+						} else {
+							$validation_url .= $saml_value;
+						}
 						
-						if (!empty($saml_value)) {
-							if (!stristr($validation_url, "?id=")) {
-								$validation_url .= "?id=" . $saml_value;
-							} else {
-								$validation_url .= $saml_value;
-							}
-							
-							// prepare cURL for the call
-							$ch = curl_init($validation_url);
-							
-							curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-							curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-							curl_setopt($ch, CURLOPT_HEADER, false);
-							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-							curl_setopt($ch, CURLOPT_TIMEOUT, 5); // wait max 5 seconds for response
-							
-							// execute the cURL call
-							curl_exec($ch);
-							$curl_info = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-							
-							// validate the reponse
-							if (!empty($curl_info) && $curl_info == 200) {
-								$result = true;
-							}
+						// prepare cURL for the call
+						$ch = curl_init($validation_url);
+						
+						curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+						curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+						curl_setopt($ch, CURLOPT_HEADER, false);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+						curl_setopt($ch, CURLOPT_TIMEOUT, 5); // wait max 5 seconds for response
+						
+						// execute the cURL call
+						curl_exec($ch);
+						$curl_info = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+						
+						// validate the reponse
+						if (!empty($curl_info) && $curl_info == 200) {
+							$result = true;
 						}
 					}
 				}
