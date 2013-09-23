2ac227ab6773b0ae7ba1259e1d10ae1ce9358776
Jerome Bakker <jeabakker@coldtrick.com>
2013-09-23 13:42:36 +0200

    added: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,15 @@
+<?php
+$language = array (
+  'vocontent_modifications:error:simplesaml' => 'Deze plugin werkt niet als de SimpleSAML niet is geactiveerd',
+  'vocontent_modifications:settings:saml_source' => 'SAML bron selectie',
+  'vocontent_modifications:settings:saml_source:description' => 'Op welke SAML bron moeten we de data verifiëren',
+  'vocontent_modifications:settings:online:validation' => 'Online validatie instellingen',
+  'vocontent_modifications:settings:online:validation:description' => 'Als de gebruiker bovenstaande combinatie niet heeft zal er een online validatie plaats vinden op het BRIN nummer van de gebruiker',
+  'vocontent_modifications:settings:online:validation_url' => 'Geeft de URL op waar het BRIN nummer kan worden geverifieerd',
+  'vocontent_modifications:settings:saml' => 'SAML atribuut validatie instellingen',
+  'vocontent_modifications:settings:saml:description' => 'Verifieer of de gebruiker een combinatie van SAML attribuut en waarde heeft',
+  'vocontent_modifications:settings:saml:attribute' => 'Geeft het SAML attribuut op om te controleren',
+  'vocontent_modifications:settings:saml:value' => 'Wat moet de waarde van het SAML attribuut zijn',
+  'vocontent_modifications:access:label' => 'VO-Content speciale leden',
+);
+add_translation("nl", $language);
